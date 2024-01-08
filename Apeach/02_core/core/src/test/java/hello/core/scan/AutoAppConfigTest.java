4f7b08c5011be6792958f2e3bda0f427cfcddad4
wooni <cloud0477@naver.com>
2024-01-08 22:14:52 +0900

    feat : [4~6강의] 테스트 소스 커밋

---
+++ Apeach/02_core/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,24 @@
+package hello.core.scan;
+
+import static org.assertj.core.api.Assertions.*;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
+    }
+}
