e812b8f8c9674a8d35febbcd4a8211ffe16d20c6
wooni <cloud0477@naver.com>
2024-01-17 20:44:10 +0900

    feat : [7~10강] 강의 학습 커밋

--- Apeach/02_core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Apeach/02_core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,14 +4,31 @@ import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
 @Component
+// @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    // 생성자가 하나라면, Autowired가 자동으로 붙는다. (생략 가능)
+    // @RequiredArgsConstructor 는 final(required) 필드 값을 기반으로 생성자를 만든다.
+    // 즉, 생성자 부분 작성 생략이 가능하다.
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
