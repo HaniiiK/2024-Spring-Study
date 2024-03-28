2854a6d4e0e1b531aecc9f0e5daf2839153e10d2
HaeunKim <rlagkdms26@gmail.com>
2024-03-28 22:40:06 +0900

    feat : [섹션 6] 강의 내용 커밋2 - 로깅

---
+++ Hi/04_MVC1/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,31 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass()); //@Slf4j가 대체해줌
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+        String name2 = "Spring2";
+
+        log.trace("trace log={}", "{}", name, name2);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+
+
+
+}
